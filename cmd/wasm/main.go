082f30f755fe8dbe33fd115b7f470b0748da52be
SamDz16 <samdz161999@gmail.com>
2022-01-01 22:26:21 +0100

    first webassembly & go hello world program

---
+++ cmd/wasm/main.go
@@ -0,0 +1,14 @@
+// Program to be copiled to webAssembly and executed in the browser
+
+package main
+
+import "fmt"
+
+// func add(a int, b int) int {
+// 	return a + b
+// }
+
+func main () {
+	// var res = add(4, 5)
+	fmt.Println("Hello, World!")
+}
