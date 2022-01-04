e93c1c4c7a4c90b6247304a75626c8bd72c1d868
SamDz16 <samdz161999@gmail.com>
2022-01-04 09:50:03 +0100

    new version

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -2,13 +2,31 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"syscall/js"
+)
 
-// func add(a int, b int) int {
-// 	return a + b
-// }
+var text string = "ceci est un texte que je veux mettre en majuscule qui est passé par webassembly"
+
+func add(a, b int) int {
+	return a + b
+}
+
+func greet(greet string) string {
+	return greet
+}
 
 func main () {
 	// var res = add(4, 5)
+
+	// inp := js.Global().Get("document").Call("findElementById", "monText")
+	// inp.value = text
+	
+	fmt.Println(text)
 	fmt.Println("Hello, World!")
+
+	js.Global().Set("greet", greet)
+	js.Global().Set("varia", text)
 }
+
